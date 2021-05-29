27759f523fcd119361c4a039ca03bbf9d65a27d2
Deko Moon <dekomoon@MacBook-Air.local>
2021-05-29 07:27:19 -0400

    doing scss

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const topNavHamBtn = document.querySelector('.top-nav__ham-button');
+const topNavBtnWrapper = document.querySelector('.top-nav__button-wrapper');
+
+topNavHamBtn.addEventListener('click', () => {
+  topNavBtnWrapper.classList.toggle('top-nav__button-wrapper--hidden');
+});
