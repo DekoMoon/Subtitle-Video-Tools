62b6c49fe14afc630a80a99cf677b0668cd051f5
Deko Moon <dekomoon@MacBook-Air.local>
2021-06-12 10:54:58 -0400

    search functionality finished

--- js/component/topNav.js
+++ js/component/topNav.js
@@ -6,7 +6,11 @@ const searchBarInp = document.querySelector('.search-bar__search-input');
 const searchBarBtn = document.querySelector('.search-bar__search-button');
 const searchBarItems = document.querySelector('.search-bar__items');
 
-const toolsName = ['fixed offset', 'linear offset', 'video analysis'];
+const tools = [
+  [ 'fixed offset', '/html/fixed-offset.html' ], 
+  [ 'linear offset', '/html/linear-offset.html' ], 
+  [ 'video analysis', '/html/video-analysis.html' ]
+];
 
 /*
 
@@ -24,20 +28,46 @@ topNavHamBtn.addEventListener('click', () => {
 topNavBtnWrapper.classList.add('top-nav__button-wrapper--hidden');
 
 
+searchBarInp.addEventListener('focus', (e) => {
+  searchBarItems.classList.remove('search-bar__items--hidden');
+});
+
 searchBarInp.addEventListener('input', (e) => {
-  const value = e.target.value;
+  const txt = e.target.value.toLowerCase();
+  const regex = new RegExp(txt, 'g');
+
+  searchBarItems.innerHTML = '';
+
+  for (let i = 0; i < tools.length; i++) {
+    const toolName = tools[i][0].toLowerCase();
+    const matched = toolName.match(regex);
+    if (matched == null) continue;
+
+
+    const capitalized = toolName
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+
+    const html = `
+    <li class="search-bar__item">
+      <a href="${tools[i][1]}" class="search-bar__link button-6">${capitalized}</a>
+    </li>
+    `;
+
+    console.log(html);
+
+    searchBarItems.insertAdjacentHTML("beforeend", html);
+  }
 });
 
 document.body.addEventListener('click', (e) => {
-  const target = e.target;
+  const path = e.path;
 
-  // TODO: Too wordy, find a way to reduce the amount. Especially for li and a
-  // switch (target) {
-  //   case searchBarInp:
-  //   case searchBarBtn:
-  //   case searchBarItems:
-  //   case searchBarItem:
-  //     case searchBar
-  // }
-})
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === searchBar) return;
+  }
+
+  searchBarItems.classList.add('search-bar__items--hidden');
+});
 
